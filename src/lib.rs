50455a24524dd1fc57f5c7de5067f33ae2c819af
LonnonjamesD <xxl3m0nk1ngxx@gmail.com>
2021-08-15 13:54:16 -0400

    Hashmap

--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,15 @@ mod parser;
 
 #[cfg(test)]
 mod tests {
+    use std::collections::HashMap;
+
+    use crate::parser::parse;
     
     #[test]
     fn it_works() {
-        
+        let mut hashmappers: HashMap<String, u64> = HashMap::new();
+        hashmappers.insert("A".to_owned(), 0);
+        hashmappers.insert("B".to_owned(), 1);
+        parse(hashmappers);
     }
 }
